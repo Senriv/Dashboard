6b85e77961e5e4fdf302c5bcde304632f687b10a
Senriv <64468958+Senriv@users.noreply.github.com>
2024-01-15 14:57:17 +0000

    Deploying to gh-pages from @ Senriv/Dashboard@0fdc80b3dde6551584d2094f2822833bbda04f63 🚀

---
+++ assets/ErrorPage-4e7db118.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ecb55276.js";function o(){return r.jsx("h1",{children:"Error Page"})}export{o as default};
--- assets/ErrorPage-571eafdc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4b0f83cf.js";function o(){return r.jsx("h1",{children:"Error Page"})}export{o as default};
